a1e072c64bb7ef8fcc18e78acafd2ef1779923ac
mfvazquez <mfvazquezfiuba@gmail.com>
2017-11-15 01:37:48 -0300

    subo las simulaciones del tp2

--- TP1/mediciones/regulacion de linea/grafico.m
+++ TP1/mediciones/regulacion de linea/grafico.m
@@ -7,6 +7,26 @@ fclose(fID);
 Vi = data{1};
 Vo = data{2};
 
+inicio = 19; % LIMITE SUPERIOR DEL MODO REG DE TENSION
+
+p= polyfit(Vi(inicio:end),Vo(inicio:end),1);
+
+Vin = 16:0.1:30;
+Vout = p(1) * Vin + p(2);
+
+20*log10(p(1))
+
+figure
+plot(Vi(inicio:end),Vo(inicio:end), 'bo');
+hold on
+plot(Vin, Vout, 'r');
+grid
+xlabel('Tensión de entrada [V]');
+ylabel('Tensión de salida [V]');
+
+
+
+
 
 figure
 plot(Vi,Vo, 'k', 'Linewidth',5);
